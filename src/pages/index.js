a5343fb65b9e2b0cdd3e1b5dd1d4cab7bf9a8f50
anas-alnabahin <anasnb2002nb@gmail.com>
2025-04-24 14:30:59 +0300

    finishing the blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import styled from "styled-components"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => {
   console.log(data)
@@ -12,11 +21,15 @@ export default ({ data }) => {
       <div>
         <h1>Anas's Thoughts</h1>
         <h4>{data.allMarkdownRemark.totalCount}</h4>
-        {data.allMarkdownRemark.edges.map(({node})=>(
-        <div key={node.id}>
-          <span>{node.frontmatter.title}  -  {node.frontmatter.date}</span>
-          <p>{node.excerpt}</p>
-        </div>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
         ))}
       </div>
     </Layout>
@@ -26,7 +39,7 @@ export const Head = () => <Seo title="Home" />
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -35,6 +48,9 @@ export const query = graphql`
             description
             title
             date
+          }
+          fields {
+            slug
           }
           excerpt
         }
